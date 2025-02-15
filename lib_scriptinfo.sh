9cd5f55925cfa12a8a634e74775a1f2dd21d63d4
Mark Michaelis <thragor@mailbox.org>
2025-02-15 09:59:46 +0100

    feat(lib): lib_scriptinfo, First Version
    Base library for accessing script information.

---
+++ lib_scriptinfo.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+###
+### Include for script information functions.
+###
+### Usage:
+###
+###   SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+###   readonly SCRIPT_DIR
+###   source "${SCRIPT_DIR}/lib-bash/lib_scriptinfo.sh"
+###
+
+# Guard variable to prevent multiple imports
+if [[ -n "${LIB_SCRIPTINFO_SH_INCLUDED:-}" ]]; then
+  return
+fi
+LIB_SCRIPTINFO_SH_INCLUDED=1
+
+LIB_SCRIPTINFO_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+readonly LIB_SCRIPTINFO_DIR
+source "${LIB_SCRIPTINFO_DIR}/lib_init.sh"
+
+# ------------------------------------------------------------------------------
+# Get script name (without path, with extension).
+#
+# Arguments:
+#   None
+# Returns:
+#   The script name.
+# ------------------------------------------------------------------------------
+function get_script_name() {
+  echo "${0##*/}"
+}
