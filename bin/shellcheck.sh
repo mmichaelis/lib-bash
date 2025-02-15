819eef82854eab3e72881c7b4f3ea0d619cf85d2
Mark Michaelis <thragor@mailbox.org>
2025-02-15 23:27:04 +0100

    refactor(lint): Rename `lint.sh` to `shellcheck.sh`

--- bin/shellcheck.sh
+++ bin/shellcheck.sh
@@ -5,18 +5,21 @@ set -o nounset  # abort on unbound variable
 set -o pipefail # don't hide errors within pipes
 
 # Check if shellcheck is installed
-if ! command -v shellcheck &> /dev/null; then
+if ! command -v shellcheck &>/dev/null; then
   echo "Error: shellcheck is not installed." >&2
   echo "Please install shellcheck. For more information, visit: https://www.shellcheck.net/" >&2
   exit 1
 fi
 
-SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}" || true)")"
+MY_PATH="$(realpath "${BASH_SOURCE[0]}")"
+readonly MY_PATH
+SCRIPT_DIR="$(dirname "${MY_PATH}")"
 readonly SCRIPT_DIR
 
-LIB_DIR="${SCRIPT_DIR}/.."
+readonly LIB_DIR="${SCRIPT_DIR}/.."
 # Find all shell scripts in the directory
-LIBS=$(find "${LIB_DIR}" -name "*.sh")
+LIBS=$(find "${LIB_DIR}" -type f -name "*.sh" -not -path "${LIB_DIR}/tests/bats/*")
+readonly LIBS
 
 # Run shellcheck on each script
 for script in ${LIBS}; do
