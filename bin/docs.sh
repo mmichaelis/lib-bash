9be6840015a052a958596e28da86cf687888c81a
Mark Michaelis <thragor@mailbox.org>
2025-03-12 19:01:43 +0100

    ci(github): Create Scripts to Generate Docs
    Generate (rather simple) scripts to generate documentation from the
    library-files.

---
+++ bin/docs.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+###
+### Script to generate Markdown documentation for all library files.
+###
+### Invokes `doc-single.sh` for each library file `lib_*.sh`. The output is
+### written to `docs/lib`. Before processing each library file, the target
+### directory is cleared and ensures that it exists.
+###
+### Usage:
+###
+### ```bash
+### doc.sh
+### ```
+###
+
+set -o errexit  # abort on nonzero exit status
+set -o nounset  # abort on unbound variable
+set -o pipefail # don't hide errors within pipes
+
+MY_PATH="$(realpath "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "${MY_PATH}")"
+readonly MY_PATH
+readonly SCRIPT_DIR
+readonly DOC_SINGLE="${SCRIPT_DIR}/doc-single.sh"
+readonly LIB_BASH_DIR="${SCRIPT_DIR}/.."
+readonly LIB_DOCS_DIR="${SCRIPT_DIR}/../docs/lib"
+
+function main() {
+  rm -rf "${LIB_DOCS_DIR}"
+  mkdir -p "${LIB_DOCS_DIR}"
+
+  local lib
+
+  for lib in "${LIB_BASH_DIR}"/lib_*.sh; do
+    local lib_name
+    lib_name="$(basename "${lib}")"
+    local target="${LIB_DOCS_DIR}/${lib_name%.sh}.md"
+    "${DOC_SINGLE}" "${lib}" >"${target}"
+  done
+}
+
+main "${@}"
